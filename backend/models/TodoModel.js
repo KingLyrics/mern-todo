fdd567428ec2fd6b1709e21e7792c9c78bc97eaa
aBugInTheSystem <ekom338.com@gmail.com>
2023-10-31 16:30:20 +0300

    Todo Model created

--- backend/models/TodoModel.js
+++ backend/models/TodoModel.js
@@ -1,1 +1,25 @@
 const mongoose = require("mongoose");
+
+const TodoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dueDate: { Date },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const TodoModel = mongoose.model("ToDo", TodoSchema);
+
+module.exports = TodoModel;
